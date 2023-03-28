5b82bc986a61f126209349cdd211fbe8da5bf704
Grace Gao <gracegao@Graces-MacBook-Pro.local>
2023-03-28 17:24:31 -0400

    adding ui/ux features

--- app/index.js
+++ app/index.js
@@ -1,12 +1,47 @@
-import {View, Text} from 'react-native'
+import { useState } from "react";
+import { View, Text, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter } from "expo-router";
+import {menu, profile} from "../constants/icons"
+import { COLORS, icons, images, SIZES } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
 
-const Home = ()=>{
+const Home = () => {
+  const router = useRouter();
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+ <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension='60%' />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={images.profile} dimension='100%' />
+          ),
+          headerTitle: "",
+        }}
+      />
 
-    return (
-        <View>
-<Text>Home</Text>
+        <ScrollView showsVerticalScrollIndicator={false}>
+        <View
+        style={{flex:1, padding:SIZES.medium}}
+        >
+            <Welcome />
+            <Popularjobs/>
+            <Nearbyjobs/>
+
         </View>
-    )
-}
 
-export default Home
+        </ScrollView>
+
+    </SafeAreaView>
+  );
+};
+
+export default Home;
